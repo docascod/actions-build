5d50013d7ebfb26caba0ef43ed96784694011714
docsAsCode <58882117+docsAsCode@users.noreply.github.com>
2020-10-19 09:26:22 +0200

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-echo "sources:"$INPUT_SOURCES
-echo "outpuit_dir":$INPUT_OUTPUT_DIR
+DEFAULT_DESTINATION=$INPUT_OUTPUT_DIR
+DEFAULT_OUTPUT=$INPUT_DEFAULT_OUTPUTS
 
-entrypoint.sh build $*
+entrypoint.sh build $INPUT_SOURCES
+
+# echo "::set-output name=pdf::${DOCS}/document.pdf"
