54c00c6ae2fc80aed441d20630cba5484eaa024e
docsAsCode <58882117+docsAsCode@users.noreply.github.com>
2020-11-07 14:14:25 +0100

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 
-# DEFAULT_DESTINATION=$INPUT_OUTPUT_DIR
-# DEFAULT_OUTPUT=$INPUT_DEFAULT_OUTPUTS
-# echo $DEFAULT_DESTINATION
-
 entrypoint.sh build $INPUT_SOURCES
-# echo ${generatedFiles[@]}
-# echo "::set-output name=generatedFiles::${generatedFiles[@]}"
-# echo "::set-output name=pdf::${DOCS}/document.pdf"
